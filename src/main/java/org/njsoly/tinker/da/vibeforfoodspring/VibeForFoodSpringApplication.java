f9cdbc4aa2b62cee378ca2c7188c7bbd98f75f0e
njsoly <njsoly@syntj.com>
2026-07-19 21:21:48 -0500

    Initial push of spring/gradle files

---
+++ src/main/java/org/njsoly/tinker/da/vibeforfoodspring/VibeForFoodSpringApplication.java
@@ -0,0 +1,13 @@
+package org.njsoly.tinker.da.vibeforfoodspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VibeForFoodSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VibeForFoodSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/njsoly/tinker/da/vibeforfoodspring/VibeForFoodSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.njsoly.tinker.da.vibeforfoodspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VibeForFoodSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
