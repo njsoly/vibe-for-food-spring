0909215d7e54d8dbfb869a01316294c675b95276
njsoly <njsoly@syntj.com>
2026-07-24 04:19:08 -0500

    new modules shelly and trijton

---
+++ vibe-for-food-spring/trijton-260723/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm")
+}
+
+group = "org.njsoly.tinker.da"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
